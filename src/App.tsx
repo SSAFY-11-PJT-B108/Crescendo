098489c07134a89f8015cd8fc5a614db87918cf4
kwonja <kownja87@gmail.com>
2024-07-24 17:20:16 +0900

    테스트

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,7 @@
-import React from 'react';
-import './App.css';
+import React from 'react'
 
-function App() {
-  return <div className="App"></div>;
+export default function App() {
+  return (
+    <div>App</div>
+  )
 }
-
-export default App;
