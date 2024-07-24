95bcf95c4363a1ff3628969ba0f32f47ba359f6f
kwonja <kownja87@gmail.com>
2024-07-24 15:23:10 +0900

    테스트

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,6 @@ import React from 'react';
 import './App.css';
 
 function App() {
-
-  var number = 123;
-  let m=3
   return <div className="App"></div>;
 }
 
--- src/Main.tsx
+++ src/Main.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 
 export default function Main() {
-  const name = '12312312';
   return <div>Main</div>;
 }
