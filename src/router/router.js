bac24fead7c3c248e453dcf68fb9c9ecf0d838f8
kwonja <kownja87@gmail.com>
2024-07-29 08:29:13 +0900

    [STYLE] 공백변경

--- src/router/router.js
+++ src/router/router.js
@@ -1,6 +1,8 @@
-import Main from '../pages/Main/Main';
+import Main from '../pages/Main';
 import App from '../App';
 import { createBrowserRouter } from 'react-router-dom';
+import Chat from '../pages/Chat';
+import Join from '../pages/Join';
 export const router = createBrowserRouter([
   {
     path: '/',
@@ -9,6 +11,14 @@ export const router = createBrowserRouter([
       {
         path: '/',
         element: <Main />,
+      },
+      {
+        path: '/chat',
+        element: <Chat />,
+      },
+      {
+        path: '/join',
+        element: <Join />,
       },
     ],
   },
