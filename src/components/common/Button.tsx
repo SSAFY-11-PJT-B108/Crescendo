5209d8bbf5e9d60dd5bdeb790820429d7bcf82bd
kwonja <kownja87@gmail.com>
2024-07-29 09:41:39 +0900

    [FEAT] 에러페이지 구현

---
+++ src/components/common/Button.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+
+interface ButtonProps{
+    onClick: () => void;
+    children: React.ReactNode;
+    className?: string;
+}
+export default function Button({children,onClick,className} : ButtonProps) {
+  return (
+    <button onClick={onClick} className={`button ${className}`}>{children}</button>
+  )
+}
--- src/router/router.js
+++ src/router/router.js
@@ -1,10 +1,12 @@
 import Main from '../pages/Main';
 import App from '../App';
 import { createBrowserRouter } from 'react-router-dom';
+import ErrorPage from '../components/layout/ErrorPage';
 export const router = createBrowserRouter([
   {
     path: '/',
     element: <App />,
+    errorElement : <ErrorPage/>,
     children: [
       {
         path: '/',
